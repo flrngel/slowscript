d33a85da8ee391bdd68831de3f09f3540982fffe
flrngel <flrngel@gmail.com>
2013-10-23 15:41:12 +0900

    v0.2.0

--- README.md
+++ README.md
@@ -1,11 +1,32 @@
 #slowscript
 
+load javascript file after page loaded, able with AMD
+
+### Usage
+
+	<slowscript src="test.js"></slowscript>
+	<script src="slowscript.min.js"></script>
+
+### Development
+
+	npm install
+	grunt
+
+
+#slowscript
+
 load javascript after page loaded
 
 ### Usage
 
 	<slowscript src="test.js"></slowscript>
 	<script src="slowscript.min.js"></script>
+
+### On AMD ( Example with RequireJS )
+
+See more [RequireJS-example](./test/require.html)
+	
+	require(['slowscript'],function(){slowscript();});
 
 ### Development
 
--- slowscript.js
+++ slowscript.js
@@ -1,14 +1,36 @@
-(function(window,document){
-	window.addEventListener('load',function(){
-		var tags=document.getElementsByTagName("slowscript");
-		for(var i=0;i<tags.length;i++){
-			var tag=tags[i];
-			var src=tag.getAttribute("src");
-			var element=document.createElement("script");
-			element.setAttribute("src",src);
-			if( document.body ){
-				document.body.appendChild(element);
+(function(window){
+	// slowscript Definition
+	function ssDefinition(){
+		var document=window.document;
+		var slowscript=function(){
+			return new slowscript.prototype.process();
+		};
+
+		slowscript.prototype.process=function(){
+			var tags=document.getElementsByTagName("slowscript");
+			for(var i=0;i<tags.length;i++){
+				var tag=tags[i];
+				var src=tag.getAttribute("src");
+				var element=document.createElement("script");
+				element.setAttribute("src",src);
+				if( document.body ){
+					document.body.appendChild(element);
+				}
 			}
-		}
-	},false);
-})(window,document);
+		};
+
+		return slowscript;
+	}
+
+	// transport
+	// for AMD
+	if( typeof define === "function" && define.amd ){
+		define( "slowscript", [], ssDefinition);
+	}else{
+		// browser global
+		window.addEventListener('load',function(){
+			window.slowscript=ssDefinition();
+			window.slowscript();
+		});
+	}
+})(this);
