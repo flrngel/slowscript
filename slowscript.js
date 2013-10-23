60f865aec330059ac2fa5acbe689580b78b01403
flrngel <flrngel@gmail.com>
2013-10-23 22:17:25 +0900

    slowscript with stylesrc load (css stylesheet)

--- slowscript.js
+++ slowscript.js
@@ -10,11 +10,24 @@
 			var tags=document.getElementsByTagName("slowscript");
 			for(var i=0;i<tags.length;i++){
 				var tag=tags[i];
+
 				var src=tag.getAttribute("src");
 				var element=document.createElement("script");
-				element.setAttribute("src",src);
-				if( document.body ){
-					document.body.appendChild(element);
+				if( src ){
+					element.setAttribute("src",src);
+					if( document.body ){
+						document.body.appendChild(element);
+					}
+				}
+
+				var stylesrc=tag.getAttribute("stylesrc");
+				var style_element=document.createElement("link");
+				if( stylesrc ){
+					style_element.setAttribute("rel","stylesheet");
+					style_element.setAttribute("href",stylesrc);
+					if( document.head ){
+						document.head.appendChild(style_element);
+					}
 				}
 			}
 		};
