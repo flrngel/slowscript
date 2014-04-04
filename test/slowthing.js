2ab87f0fa104cca645b6f9443d37bce445ac8c7c
flrngel <flrngel@gmail.com>
2014-04-04 12:43:18 +0900

    v2(v1.0.0) first commit

---
+++ test/slowthing.js
@@ -0,0 +1,6 @@
+(function(window){
+	var document=window.document;
+	var it=document.createElement("div");
+	it.innerHTML="this is just start of sexy thing";
+	document.body.appendChild(it);
+})(window);
